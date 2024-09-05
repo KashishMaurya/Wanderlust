75f8408839e90e8b444aed62dcd8491b7e4c0317
Kashish Maurya <kashish.social.18@gmail.com>
2024-09-05 20:53:13 +0530

    Add Project files

---
+++ public/js/map.js
@@ -0,0 +1,20 @@
+// map in listings
+
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  center: listing.geometry.coordinates, // starting position [lng, lat]
+  style: "mapbox://styles/mapbox/streets-v9",
+  zoom: 9, // starting zoom
+});
+
+// Create a new marker.
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates) //listing/geometry / coordinate
+  .setPopup(
+    // text on hovering pin in map
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h4>${listing.title}</h4><p>Exact location provided after booking!</p>`
+    )
+  )
+  .addTo(map);
